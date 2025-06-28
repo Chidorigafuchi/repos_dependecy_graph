f1e22a62b2d9c635e4aa775a62834b9086636a68
Chidorigafuchi <maksimtell@mail.ru>
2025-06-29 01:46:13 +0300

    Теперь ответ в виде словарей  по ключам package_to_package, set_to_package, package_to_library

--- yum_parser/services/graph.py
+++ yum_parser/services/graph.py
@@ -1,166 +1,112 @@
-import re
 from .parser import parse_packages
 
 
 class Graph:
     packages = {}
     current_package = ''
+    saved_packages_graph = {}
 
     @classmethod
     def get_package_graph(cls, pkg_name):
         parent_packages = []
         children_packages = []
-        packages_graph = {}
 
         if not cls.packages:
             cls.packages = parse_packages()
 
-        if (pkg_name in cls.packages.keys()):
-            pkg = cls.packages[pkg_name]
-            #тут информация о пакете
-        else:
-            return packages_graph
-
+        if (pkg_name not in cls.packages.keys()):
+            return {}
+                    
         if pkg_name != cls.current_package:
             cls.current_package = pkg_name
-            packages_graph = {}
-        #При переходе на недалекую вершину не удалять всё, а только ставшие ненужными вершины
-        
-        # Добавить обработку библиотек с пробелом в названии
-        # for package_name in cls.packages.keys():
-        #     for lib in cls.packages[package_name][0]:
-        #         lib_name = lib[0]
-        #         if ' ' in lib_name:
-        #             # print('Нашли необычную библиотеку:')
-        #             print(lib_name, package_name)
+            packages_graph = {
+                'package_to_package': {},
+                'set_to_package': {},
+                'package_to_library': {},
+            }
+            cls.saved_packages_graph = {}
+        else:
+            return cls.saved_packages_graph
 
-        parent_packages, packages_graph = cls.get_package_parents(pkg_name, packages_graph)
+        parent_packages, packages_graph = cls.get_package_neighbours(pkg_name, packages_graph, up=True)
 
-        children_packages, packages_graph = cls.get_package_childrens(pkg_name, packages_graph)
-
-        # print('\nРодительские пакеты:\n', parent_packages)
-
-        # print('\nДочерние пакеты:\n', children_packages)
-
-        # print('\n\n')
-        # for key in packages_graph.keys():
-        #     print(f'{key}: {packages_graph[key]}')
-
-        grand_parent_packages = []
+        children_packages, packages_graph = cls.get_package_neighbours(pkg_name, packages_graph, up=False)
 
         for package in parent_packages:
-            parent_packages, packages_graph = cls.get_package_parents(package, packages_graph)
-            grand_parent_packages += parent_packages
+            parent_packages, packages_graph = cls.get_package_neighbours(package, packages_graph, up=True)
 
-        grand_children_packages = []
         for package in children_packages:
-            children_packages, packages_graph = cls.get_package_childrens(package, packages_graph)
-            grand_children_packages += children_packages
+            children_packages, packages_graph = cls.get_package_neighbours(package, packages_graph, up=False)
+
+        cls.saved_packages_graph = packages_graph
 
         return packages_graph
     
     @classmethod
-    def get_package_parents(cls, pkg_name, packages_graph):
-        libs_parents = []
-        parent_packages = []
-        pkg = cls.packages[pkg_name]
-        pkg_requires = pkg[0]
+    def get_package_neighbours(cls, current_package, packages_graph, up): #Переименовать
+        dependecies = []
+        neighbours = []
+        pkg = cls.packages[current_package]
+        package_dependencies = pkg[0] if up else pkg[1]
         
-        for lib in pkg_requires:
-            lib_name = lib[0]
-            if lib_name not in libs_parents:
-                libs_parents.append(lib_name)
+        for dependency in package_dependencies:
+            dependency_name = dependency[0]
+            if dependency_name not in dependecies:
+                dependecies.append(dependency_name)
         
-        libs_parents, parent_packages, packages_graph = cls.find_parents_packages(libs_parents, pkg_name, packages_graph)
+        neighbours, packages_graph = cls.find_package_neighbours(dependecies, current_package, packages_graph, up)
 
-        # print('\nНеиспользоваенные библиотеки родителей:')
-        # for lib in libs_parents.keys():
-        #     if not libs_parents[lib]:
-        #         print(lib)
-        
-        return parent_packages, packages_graph
-
-    @classmethod
-    def get_package_childrens(cls, pkg_name, packages_graph):
-        libs_childrens = []
-        children_packages = []
-        pkg = cls.packages[pkg_name]
-        pkg_provides = pkg[1]
-
-        for lib in pkg_provides:
-            lib_name = lib[0]
-            if lib_name not in libs_childrens:
-                libs_childrens.append(lib_name)
-
-        libs_childrens, children_packages, packages_graph = cls.find_children_packages(libs_childrens, pkg_name, packages_graph)
-
-        # print('\nНеиспользоваенные библиотеки детей:')
-        # for lib in libs_childrens.keys():
-        #     if not libs_childrens[lib]:
-        #         print(lib)
-        
-        return children_packages, packages_graph
-
-    @classmethod
-    def find_parents_packages(cls, libs_parents, dependent_package, packages_graph):
-        parent_packages = []
-        libs_packages = {}
-
-        for lib_name in libs_parents:
-            libs_packages[lib_name] = []
-        
-        for package_name in cls.packages.keys(): 
-            for lib in cls.packages[package_name][1]: #по provides
-                lib_name = lib[0]
-                if lib_name in libs_parents:
-                    if package_name not in parent_packages:
-                        parent_packages.append(package_name)
-                        libs_packages[lib_name].append(package_name)
-
-        for lib in libs_packages.keys():
-            packages = libs_packages[lib]
-            if (packages):
-                packages_graph = cls.add_dependence(packages, [dependent_package], packages_graph)
-
-        return libs_packages, parent_packages, packages_graph
+        return neighbours, packages_graph
     
     @classmethod
-    def find_children_packages(cls, libs_childrens, parent_package, packages_graph):
-        children_packages = []
-        libs_packages = {}
+    def find_package_neighbours(cls, dependecies, current_package, packages_graph, up):
+        neighbours = []
+        dependecies_neighbours = {}
 
-        for lib_name in libs_childrens:
-            libs_packages[lib_name] = []
-
+        for dependecy_name in dependecies:
+            dependecies_neighbours[dependecy_name] = []
+        
         for package_name in cls.packages.keys(): 
-            for lib in cls.packages[package_name][0]: #по requires
-                lib_name = lib[0]
-                if lib_name in libs_childrens:
-                    if package_name not in children_packages:
-                        children_packages.append(package_name)
-                        libs_packages[lib_name].append(package_name)
-                
-        for lib in libs_packages.keys():
-            packages = libs_packages[lib]
-            if (packages):
-                packages_graph = cls.add_dependence([parent_package], packages, packages_graph)
+            package_dependencies = cls.packages[package_name][1] if up else cls.packages[package_name][0]
+            for dependecy in package_dependencies:
+                dependecy_name = dependecy[0]
+                if dependecy_name in dependecies:
+                    if package_name not in neighbours:
+                        neighbours.append(package_name)
+                        dependecies_neighbours[dependecy_name].append(package_name)
 
-        return libs_packages, children_packages, packages_graph
+        for dependency in dependecies_neighbours.keys():
+            packages = dependecies_neighbours[dependency]
+            if (packages):
+                if up:
+                    packages_graph = cls.add_dependence(packages, [current_package], packages_graph)
+                else:
+                    packages_graph = cls.add_dependence([current_package], packages, packages_graph)
+            else:
+                if up:
+                    if (current_package in packages_graph['package_to_library'].keys()):
+                        packages_graph['package_to_library'][current_package].append(dependency)
+                    else:
+                        packages_graph['package_to_library'][current_package] = [dependency]
+        
+
+
+        return neighbours, packages_graph
 
     def add_dependence(main_packages, dependent_packages, packages_graph):
         if (len(main_packages) == 1):
             package_name = main_packages[0]
-            if (package_name in packages_graph):
+            if (package_name in packages_graph['package_to_package'].keys()):
                 for dependent_pkg in dependent_packages:
-                    if dependent_pkg not in packages_graph[package_name]:
-                        packages_graph[package_name].append(dependent_pkg)
+                    if dependent_pkg not in packages_graph['package_to_package'][package_name]:
+                        packages_graph['package_to_package'][package_name].append(dependent_pkg)
             else:
-                packages_graph[package_name] = dependent_packages
+                packages_graph['package_to_package'][package_name] = dependent_packages
         else:
-            package_name = 'SET_' + main_packages[0]
-            if (package_name in packages_graph):
-                packages_graph[package_name] += dependent_packages
+            package_name = 'SET_' + main_packages[0][:-3]
+            if (package_name in packages_graph['set_to_package'].keys()):
+                packages_graph['set_to_package'][package_name] += dependent_packages
             else:
-                packages_graph[package_name] = dependent_packages
+                packages_graph['set_to_package'][package_name] = dependent_packages
         
         return packages_graph
--- yum_parser/services/parser.py
+++ yum_parser/services/parser.py
@@ -1,25 +1,28 @@
 import createrepo_c
 
 
-#Парсим нужную информацию о пакетах из определеннго репозитория (по выбору(добавить))
 def parse_packages():
     packages = {}
-    repo = 'os'
-    repo_url = 'https://repo1.red-soft.ru/redos/8.0/x86_64/' + repo + '/'
-    repodata = createrepo_c.Metadata()
-    repodata.locate_and_load_xml(repo_url)
+    repos = ['os'] # , 'updates', 'debuginfo', 'kernel-rt', 'kernel-testing'
 
-    for key in repodata.keys():
-        pkg = repodata.get(key)
+    for repo in repos:
+        repo_url = 'https://repo1.red-soft.ru/redos/8.0/x86_64/' + repo + '/'
+        repodata = createrepo_c.Metadata()
+        repodata.locate_and_load_xml(repo_url)
 
-        pkg_info = [
-            pkg.requires,
-            pkg.provides,
-            pkg.nevra(),
-            pkg.version,
-            pkg.release,
-            pkg.url
-        ]
-        packages[pkg.name] = pkg_info
+        for key in repodata.keys():
+            pkg = repodata.get(key)
+
+            pkg_info = [
+                pkg.requires,
+                pkg.provides,
+                pkg.nevra(),
+                pkg.version,
+                pkg.release,
+                pkg.url
+            ]
+            packages[pkg.name] = pkg_info
+
+        del repodata
 
     return packages
