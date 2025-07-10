73fd013011abb827b1432ea609a109c3d8a652e0
Chidorigafuchi <maksimtell@mail.ru>
2025-07-10 17:14:25 +0300

    изменил импорт на относительные пути. Добавил entrypoint.sh и start_celery_beat.sh +x

--- backend/yum_parser/management/commands/add_repo_paths.py
+++ backend/yum_parser/management/commands/add_repo_paths.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from backend.yum_parser.utils.add_repo import add_repo_path
+from yum_parser.utils.add_repo import add_repo_path
 
 
 class Command(BaseCommand):
